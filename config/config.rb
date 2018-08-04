f645ff3362bb082e112a76d69b0337dfb53e2728
Motoyuki OHMORI <ohmori@tottori-u.ac.jp>
2018-08-05 00:00:47 +0900

    integrate sample password file into configuration file.

--- config/config.rb
+++ config/config.rb
@@ -1,4 +1,23 @@
 #
+# possible usernames, login passwords and enable passwords.
+#
+USERS = [
+	'hogehoge',
+	'fugafuga',
+	'bakeratta',
+	]
+PASSWORDS = [
+	'secret1',
+	'secret2',
+	'secret3',
+	]
+ENABLES = [
+	'secret4',
+	'secret5',
+	'secret6',
+	]
+
+#
 # Logging directory to which debug messages go.
 #
 LOGDIR = 'log'
